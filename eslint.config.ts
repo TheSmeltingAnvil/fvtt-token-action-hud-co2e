7fca46aba6aa66695e601b6b0d75270abf558682
Lionel Lalande <lionel.lalande+github@lsquared.fr>
2026-08-23 20:05:06 +0200

    chore: update eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -9,7 +9,7 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: globals.browser, parserOptions: { tsconfigRootDir: import.meta.dirname } },
   },
   tslint.configs.recommended,
   tslint.configs.strict,
